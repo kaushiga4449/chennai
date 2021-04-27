b3682dfefe97ecbaccdbedb09582aede02fc0db0
kaushigas <kaushigas@MAVBCP2100.mavericsystems.lan>
2021-04-27 13:25:23 +0530

    red27/04/2021

--- Finaldemo/src/Reddemo/Redclass.java
+++ Finaldemo/src/Reddemo/Redclass.java
@@ -2,4 +2,12 @@ package Reddemo;
 
 public class Redclass {
 
+public static void main(String[] args)
+{
+	System.out.println("hi");
+	System.out.println("welcome");
+    System.out.println("hi");
+    System.out.println("welcomehome");
 }
+}
+
